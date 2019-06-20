ad2c8183aa7d1b3622301e177d1015d1a6896063
Sara Ben Shabbat <49598750+SaraBenShabbat@users.noreply.github.com>
2019-06-20 21:30:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DetectingAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            userPicture = capture.QueryFrame().ToImage<Gray, byte>();
+        }
+    }
+}
